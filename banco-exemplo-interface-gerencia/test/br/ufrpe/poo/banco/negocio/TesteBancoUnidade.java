02802a6f221bdb7378ecc10e0fbfabd5c296ece2
Gabriel Souza <gabrielsouza943@outlook.com>
2023-12-08 14:23:35 -0300

    Criando testes JUnit p/ pacote negócios(83,4%)

--- banco-exemplo-interface-gerencia/test/br/ufrpe/poo/banco/negocio/TesteBancoUnidade.java
+++ banco-exemplo-interface-gerencia/test/br/ufrpe/poo/banco/negocio/TesteBancoUnidade.java
@@ -1,10 +1,9 @@
 package br.ufrpe.poo.banco.negocio;
 
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
